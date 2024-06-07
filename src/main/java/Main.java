15cafc255b3d87ee0aa43723a7679799af245683
Benjamin Rojo <benjaminrsalgado@gmail.com>
2024-06-07 10:50:26 -0600

    i push the proyect that i made a calculator

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author benja
+ */
+public class Main {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new CalculadoraUI().setVisible(true);
+            }
+        });
+    }
+}
